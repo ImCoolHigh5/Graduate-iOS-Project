f99f4ed754761fffb46825ebedbb7357cdffb759
ImCoolHigh5 <imcoolhigh5@gmail.com>
2014-04-12 19:36:06 -0400

    Updated Guardian View

--- Students/GuardiansViewController.m
+++ Students/GuardiansViewController.m
@@ -12,7 +12,7 @@
 
 @interface GuardiansViewController ()
 
-@property (nonatomic, strong) GuardianDataController *guardianDC;
+@property (nonatomic, strong) NSArray *guardians;
 
 @end
 
@@ -33,9 +33,7 @@
 	// Update the user interface for the detail item.
 	
 	if (self.selectedStudent) {
-		_schedule = [ScheduleDataController createScheduleForScheduleID:_selectedStudent.scheduleID];
-		//		[_selectedStudent setUpSchedule];
-		
+		_guardians = [GuardianDataController getGuardiansForStudentWithGuardianIDArray:_selectedStudent.guardianIDArray];
 		
 	}
 }
@@ -59,7 +57,105 @@
 
 #pragma mark - Table View
 
-// Grouping isn't used
+// Returns number of guardian items to be displayed
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	// take a count of the internal schedule array
+	return [_guardians count];
+}
+
+// This may need to be more extensive but for now there room for Gender First Last, Phone number, house# and street, and City State Zipcode
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return 5;
+}
+
+// Loads the cells into the sections
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+
+	Guardian *guardianItem = [_guardians objectAtIndex:indexPath.section]; // Guardian object used for this section
+	// Strings for BOOLS used to make titles (Main Contact / Emergency Contact / Resident)
+	NSString *isMain = [[NSString alloc] initWithSttring:@""];
+	NSString *isEmergency = [[NSString alloc] initWithSttring:@""];
+	NSString *isResident = [[NSString alloc] initWithSttring:@""];
+	BOOL slashNeeded = NO; // Is a Main/Emergency and Resident
+	
+	if (guardianItem.isMainContact) {
+		isMain = @"Main Contact ";
+		slashNeeded = YES;
+	} else if (guardianItem.isEmergencyContact) {
+		isEmergency = @"Emergency Contact ";
+		slashNeeded = YES;
+	}
+	if (guardianItem.isMainContact && guardianItem.isEmergencyContact) {
+		return nil;
+	}
+	if (guardianItem.isResidenceOfStudent ) {
+		if (slashNeeded) {
+			isResident = @"/ Resident"
+		} else isResident = @"Resident"
+	NSString *sectionName = [[NSString alloc] init];
+	
+	sectionName = [NSString stringWithFormat:@"%@%@%@", isMain, isEmergency, isResident];
+
+
+	return sectionName;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+
+	// Makes a new guardianItem from the student's guardianIDArray at the index specified by the tableView
+	Guardian *guardianItem = [_guardians objectAtIndex:indexPath.section];
+
+	NSString *cellTitle = [[NSString alloc] init];
+	NSString *namePrefix = [[NSString alloc] initWithString:[self stringForGender:guardianItem.isMale]];
+	// Tests to see which element needs to be pulled, depending on the cell in this section
+	// Assumes only five cells are needed per section
+	switch (indexPath.row) {
+		case 0:
+			cellTitle = [NSString stringWithFormat:@"%@ %@ %@", namePrefix, guardianItem.firstName, guardianItem.lastName];
+			break;
+		case 1:
+			cellTitle = [NSString stringWithFormat:@"Phonenumber: %@", guardianItem.phoneNumber];
+			break;
+		case 2:
+			cellTitle = [NSString stringWithFormat:@"%Address:", guardianItem.houseNumber, guardianItem.street];
+			break;	
+		case 3:
+			cellTitle = [NSString stringWithFormat:@"%@ %@", guardianItem.houseNumber, guardianItem.street];
+			break;
+		case 4:
+			cellTitle = [NSString stringWithFormat:@"%@, %@ %i", guardianItem.city, guardianItem.state, guardianItem.zipCode];
+			break;
+		default:
+			cellTitle = @"Oops!";
+			break;
+	}
+
+	cell.textLabel.text = cellTitle;
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return NO;
+}
+
+// To indicate gender withouth the need for an extra line
+-(NSString*)stringForGender:(BOOL)isMale {
+
+	if (isMale) {
+		return @"Mr.";
+	} else {
+		return @"Ms.";
+	}
+
+}
+
+
+/* // Grouping isn't used
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
 	return 1;
@@ -96,5 +192,5 @@
 //        [[segue destinationViewController] setSelectedStudent:student];
 //    }
 //}
-
+*/
 @end
